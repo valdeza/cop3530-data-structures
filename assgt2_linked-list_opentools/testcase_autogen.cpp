743baa3ca3b0deef7580a465a90231270671cd40
valdeza <valdeza@users.noreply.github.com>
2016-01-27 19:05:51 -0500

    Documentation
    Reformatted code to make it easier to read, wrote documentation, and
    made some binary files for those who like running arbitrary code.

--- assgt2_linked-list_opentools/testcase_autogen.cpp
+++ assgt2_linked-list_opentools/testcase_autogen.cpp
@@ -1,87 +1,88 @@
-/**
- * testcase_autogen.cpp
- * Generates test cases for use on that thing due Friday (29 Jan)!
- * Initial author: C. Martin
- *
- */
- 
-//TODO
-// 1. Comment code and figure out workflow.
-// 1.5. Write a README.md. Don't forget to mention that we make no guarantees that this autogen will generate test cases that can be processed in this century.
-// 1.8. Perform naive benchmarking to estimate when the worst test case *might* finish.
-// 2. Restructure as necessary.
-// 3. Change file output to console output
-//  (This way, if people want to make a file, they can just redirect output to their own specified file.)
-// 4. Implement program arguments to allow user to specify maximum end list.
 
-#include<unistd.h>
-#include<fstream>
-#include<stdlib.h>
-#include<iostream>
-int main(int, char**){
-srand ( time(NULL) );
-std::ofstream myfile;
-  myfile.open("input.txt");
-int size = 0;
-while(size<=0){
-size = rand()% 100;
-}
-myfile<<size<<std::endl;
+ //TODO
+ // 1. Comment code and figure out workflow.
+ // 1.5. Write a README.md. Don't forget to mention that we make no guarantees that this autogen will generate test cases that can be processed in this century.
+ // 1.8. Perform naive benchmarking to estimate when the worst test case *might* finish.
+ // 2. Restructure as necessary.
+ // 3. Change file output to console output
+ //  (This way, if people want to make a file, they can just redirect output to their own specified file.)
+ // 4. Implement program arguments to allow user to specify maximum end list.
 
-int *list=new int[size];
+#include <fstream>
+#include <stdlib.h>
+#include <ctime>
+#include <iostream>
 
- 
-for(int i = 0 ; i < size;i++){
-if(i+1<size){
-list[i]= rand()%200;
-myfile<<list[i]<<" ";
+int main(int argc, char** argv) {
+	// Initialisation
+	std::srand(std::time(0));
+	std::ofstream myfile;
+	myfile.open("input.txt");
 
-}
-else
-{
-list[i]= rand()%200;
-myfile<<list[i];
-}
-}
+	int size = 0; // "the number of elements in the initial list"
+	while (size <= 0) {
+		size = std::rand() % 100;
+	}
+	myfile << size << std::endl;
 
-int addi=0;
-while(addi <= 0){
-addi = rand()%20;
-}
-myfile<<std::endl<<addi;
-int *list1=new int[addi];
+	int *list = new int[size]; // "the elements in the initial list"
 
-int step =0;
-while (step <=0)
-{
-step= rand()%4;
+	for (int i = 0; i < size; i++) {
+		if (i + 1 < size) {
+			list[i] = rand() % 200;
+			myfile << list[i] << " ";
 
-}
-//myfile<<step<<std::endl;
-int sizeaddi=0;
-while(sizeaddi <= 0 &&(step * addi)<size ){
-sizeaddi = rand()%19;
-}
-//myfile<<sizeaddi<<std::endl;
-for(int i = 0; i<addi; i++){
-myfile<<std::endl<<step<<std::endl;
+		}
+		else
+		{
+			list[i] = rand() % 200;
+			myfile << list[i];
+		}
+	}
+
+	int addi = 0; // "number of additional lists"
+	while (addi <= 0) {
+		addi = rand() % 20;
+	}
+	myfile << std::endl << addi;
+	int *list1 = new int[addi]; // Additional List: "the elements"
+
+	int step = 0; // Additional List: "step"
+	while (step <= 0)
+	{
+		step = rand() % 4;
+
+	}
+	//myfile<<step<<std::endl;
+	int sizeaddi = 0; // Additional List: "number of elements"
+	while (sizeaddi <= 0 && (step * addi) < size) {
+		sizeaddi = rand() % 19;
+	}
+	//myfile<<sizeaddi<<std::endl;
+	for (int i = 0; i < addi; i++) {
+		myfile << std::endl << step << std::endl;
 
 
-myfile<<sizeaddi<<std::endl;
-for(int j = 0 ; j < sizeaddi;j++){
-if(j+1<sizeaddi ){
-list1[j]=rand()%200;
-myfile<<list1[j]<<" ";
-} else
-{
-list1[j]=rand()%200;
-myfile<<list1[j];
+		myfile << sizeaddi << std::endl;
+		for (int j = 0; j < sizeaddi; j++) {
+			if (j + 1 < sizeaddi) {
+				list1[j] = rand() % 200;
+				myfile << list1[j] << " ";
+			}
+			else
+			{
+				list1[j] = rand() % 200;
+				myfile << list1[j];
 
-}
-}
+			}
+		}
 
 
-}
-myfile<<std::endl;
-myfile.close();
+	}
+	myfile << std::endl;
+
+	// Cleanup
+	myfile.close();
+	delete[] list;
+	delete[] list1;
 }
